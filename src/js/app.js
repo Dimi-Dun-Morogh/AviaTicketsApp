43847de965f98f0f43940acce0f33cd1d41d79cb
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-05-21 18:51:24 +0300

    test git pre hook

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 import locations from './store/locations';
 locations.init().then((res) => {
-  console.log(res);
-  console.log(locations);
+  console.log(res)
+  console.log(locations)
   console.log(locations.getCitiesByCountryCode("GB"))
 });
