b009f087ef6066c1bc343946088ba493ae89f791
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-05-21 18:05:22 +0300

    implemented store module for cities and countries + filter cities by Country Code

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
-import api from './services/apiService';
-api.countries().then( res => console.log(res))
-api.cities().then( res => console.log(res))
+import locations from './store/locations';
+locations.init().then((res) => {
+  console.log(res);
+  console.log(locations);
+  console.log(locations.getCitiesByCountryCode("GB"))
+});
